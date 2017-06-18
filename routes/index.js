b3bf1c23386bc0e90c979568ece608f753baa7a4
tylertaylor <vikingtyty@gmail.com>
2017-06-18 13:30:22 -0500

    setup /api/whoami route, successfully returns JS object containing the browsers ip address, language, and operating system

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/whoami', (req, res) => {
+  // get the IP address, language and operating system for this browser
+  var osInfo = req.headers['user-agent']
+  osInfo = osInfo.split(/[\(\)]/)[1] // search for words in parentheses
+
+  var ip = req.ip
+  // we only need the first part of the language, before the comma
+  var language = req.headers['accept-language'].split(',')[0].trim()
+
+  var data = {
+    "ipaddress": ip,
+    "language": "en-US",
+    "software": osInfo
+  }
+
+  res.json(data)
+})
+
 module.exports = router;
